b6a2814c34c8345ab99890de0dc8db64b2b18367
mydevtank <mydevtank@gmail.com>
2025-11-16 22:26:46 +0800

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          750: '#2d3748',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
