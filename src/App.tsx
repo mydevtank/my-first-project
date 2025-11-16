2a5db905a3bfbc91e233b8ccf47c2b081ec4bb01
mydevtank <mydevtank@gmail.com>
2025-11-16 22:23:54 +0800

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './components/ThemeProvider';
+import Settings from './pages/Settings';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/settings" element={<Settings />} />
+          {/* Add other routes here */}
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
